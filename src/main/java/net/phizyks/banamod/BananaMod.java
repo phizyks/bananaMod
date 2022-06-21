7f2c1d9e840e0717e1667522c29504e32128f665
phizyks <leopol192@yahoo.com>
2022-06-20 21:33:24 -0400

    Initial Commit

---
+++ src/main/java/net/phizyks/banamod/BananaMod.java
@@ -0,0 +1,15 @@
+package net.phizyks.banamod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BananaMod implements ModInitializer {
+	public static final String MOD_ID = "banamod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
